a08cbc882fce284ffb1d43a7d7c6e20b3cc44db3
sarthakspiker <spikerstriker@gmail.com>
2017-04-20 15:37:54 +0530

    Initial commit

---
+++ app/src/main/java/com/test/striker/sound/MainActivity.java
@@ -0,0 +1,35 @@
+package com.test.striker.sound;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.weekends);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            mediaPlayer.start();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+    }
+}
